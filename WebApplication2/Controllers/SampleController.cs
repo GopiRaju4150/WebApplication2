dec81ac8e7960f5e1ecf891ea19a25b9498114f2
hp <hp@DESKTOP-LNEME22>
2024-10-27 12:54:52 -0700

    Add project files.

---
+++ WebApplication2/Controllers/SampleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        
+        [HttpGet]
+        [Route("GetStringData")]//Chield Routes
+        public string ListOfData()
+        {
+            var Results = "it department";
+            return Results;
+        }
+        
+        [HttpPost]
+        [Route("insertData")]//Chield Routes
+        public int Insert(int a, int b)
+        {
+            return a + b;
+        }
+       
+        [HttpPut]
+        [Route("updateData")]//Chield Routes
+        public int Update(int a, int b)
+        {
+            return a - b;
+        }
+       
+        [HttpDelete]
+        [Route("deleteData")]//Chield Routes
+        public int Delete(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
